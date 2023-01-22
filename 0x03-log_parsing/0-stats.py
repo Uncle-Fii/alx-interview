b7458ad8951f433e1735e7b891c7abe9b3411147
Uncle-Fii <fkankrumah@gmail.com>
2023-01-22 15:50:00 -0800

    removed whitespaces

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -40,10 +40,10 @@ def print_statistics(total_file_size, status_codes_stats):
 
 def update_metrics(line, total_file_size, status_codes_stats):
     '''Updates the metrics from a given HTTP request log.
-    
+
     Args:
         line (str): The line of input from which to retrieve the metrics.
-    
+
     Returns:
         int: The new total file size.
     '''
